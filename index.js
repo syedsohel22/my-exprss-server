26d486eb413278ec7782fe35a33beb7a2014c3c5
sohel <mr.sohelsyed@gmail.com>
2023-06-11 23:18:36 +0530

    added cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
+const cors = require("cors");
 const connection = require("./config/db");
 const userRouter = require("./routes/user.router");
 const noteRouter = require("./routes/note.router");
 app.use(express.json());
+app.use(cors());
 app.use("/user", userRouter);
 app.use("/note", noteRouter);
 
